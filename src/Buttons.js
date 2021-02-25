25e2a8aa4fe2806c8856d14a8bcda439658b3bb3
ogmbgm <shells.sims1@gmail.com>
2021-02-24 18:38:01 -0800

    CreateBasicPrototype

---
+++ src/Buttons.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+// import './Text.css';
+
+class Buttons extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        console.log(this.props.url)
+        return(
+            <div>
+                <a className="btn btn-primary float-start" href = {this.props.url} id="tweet-quote"><i className="fa fa-twitter"/></a>
+                <button type="button" className="btn btn-primary float-end" id="new-quote" onClick={this.props.handleSubmit}>New Quote!</button>
+            </div>
+        );
+    }
+}
+
+export default Buttons;
---
+++ src/Text.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Text.css';
+
+class Text extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="mb-3">
+                <h5 className="card-title" id="text"><i className="fa fa-quote-left"/>{`${this.props.text}"`}</h5>
+                <p className="card-text text-end" id="author">{`- ${this.props.author}`}</p>
+            </div>
+        );
+    }
+}
+
+export default Text;
